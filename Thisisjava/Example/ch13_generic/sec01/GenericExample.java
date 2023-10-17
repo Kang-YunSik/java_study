268915bacb5e5a8e233ebb0ce8f5b6c6af3b995c
kys <kang_ys_1@naver.com>
2023-10-17 13:57:09 +0900

    제네릭의 기본 기능: 객체에 여러 타입을 줄 수 있다.

--- Thisisjava/Example/ch13_generic/sec01/GenericExample.java
+++ Thisisjava/Example/ch13_generic/sec01/GenericExample.java
@@ -4,11 +4,13 @@ public class GenericExample {
     public static void main(String[] args) {
 
         //Box를 생성할 때 타입 파라미터 T대신 String으로 대체
+        // box1 객체 String을 타입으로 받는다
         Box<String> box1 = new Box<>();
         box1.content = "안녕하세요";
         String str = box1.content;
         System.out.println(str);
 
+        // box2 객체는 Integer를 타입으로 받는다.
         Box<Integer> box2 = new Box<>();
         box2.content = 100;
         Integer value = box2.content;
