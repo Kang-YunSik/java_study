6ae4fb8d243157ffb798f202a149a569b16e4b2f
kys <kang_ys_1@naver.com>
2023-10-18 17:19:08 +0900

    ArrayList와 LinkedList의 삽입 속도 차이

--- Thisisjava/Example/ch15_Collection/sec02/example03/LinkedListExample.java
+++ Thisisjava/Example/ch15_Collection/sec02/example03/LinkedListExample.java
@@ -17,6 +17,7 @@ public class LinkedListExample {
         // ArrayList 컬렉션에 저장하는 시간 측정
         startTime = System.nanoTime();
         for (int i=0; i<10000; i++){
+            // ArrayList[0]에 시간 값을 계속 추가함.(빈번한 삽입) -> 인덱스가 계속 밀려남
             list1.add(0, String.valueOf(i));
         }
         endTime = System.nanoTime();
@@ -25,6 +26,7 @@ public class LinkedListExample {
         // LinkedList 컬렉션에 저장하는 시간 측정
         startTime = System.nanoTime();
         for (int i=0; i<10000; i++){
+            // LinkedList[0]에 시간 값을 계속 추가하지만, 링크로 연결되어 있어 삽입에서 ArrayList보다 성능이 좋다.
             list2.add(0, String.valueOf(i));
         }
         endTime = System.nanoTime();
